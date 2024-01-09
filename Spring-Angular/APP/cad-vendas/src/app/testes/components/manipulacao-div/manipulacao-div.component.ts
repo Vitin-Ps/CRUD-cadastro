7a65477cbeff171dc7dbb8aee049d76ead13fbb6
Vitin-Ps <vicssoares2802@gmail.com>
2024-01-09 11:19:05 -0300

    testes-com-manipulação-div-finalizado

--- Spring-Angular/APP/cad-vendas/src/app/testes/components/manipulacao-div/manipulacao-div.component.ts
+++ Spring-Angular/APP/cad-vendas/src/app/testes/components/manipulacao-div/manipulacao-div.component.ts
@@ -1,19 +1,45 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-manipulacao-div',
   templateUrl: './manipulacao-div.component.html',
-  styleUrl: './manipulacao-div.component.css',
+  styleUrls: ['./manipulacao-div.component.css'],
 })
-export class ManipulacaoDivComponent {
+export class ManipulacaoDivComponent implements OnInit{
   @ViewChild('minhaDiv') minhaDiv!: ElementRef;
 
-  adicionarClasseEObterAltura() {
-    // Adiciona uma classe à div
-    this.minhaDiv.nativeElement.classList.add('minha-classe-adicional');
+  constructor(private renderer: Renderer2) {}
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.verificarAltura()
+    },10)
+  }
 
-    // Obtém a altura da div
-    const altura = this.minhaDiv.nativeElement.offsetHeight;
-    console.log('Altura da div:', altura);
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.verificarAltura();
+  }
+
+  adicionarClasseEObterAltura() {
+    this.renderer.addClass(this.minhaDiv.nativeElement, 'minha-classe-adicional');
+
+    setTimeout(() => {
+      this.renderer.removeClass(this.minhaDiv.nativeElement, 'minha-classe-adicional');
+    }, 4000);
+
+    this.verificarAltura();
+  }
+
+  verificarAltura() {
+    const alturaDaDiv = this.minhaDiv.nativeElement.offsetHeight;
+
+    if (alturaDaDiv > 500) {
+      this.renderer.setStyle(this.minhaDiv.nativeElement, 'background-color', 'red');
+    } else {
+      this.renderer.removeStyle(this.minhaDiv.nativeElement, 'background-color');
+    }
+
+    console.log('Altura da div:', alturaDaDiv);
   }
 }
